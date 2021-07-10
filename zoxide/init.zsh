f7ae534ff2f82d926b7057c536be78e21f238f8d
QIQI <qiqi@f2pool.com>
2021-07-10 07:42:39 +0000

    update

---
+++ zoxide/init.zsh
@@ -0,0 +1,22 @@
+eval "$(zoxide init zsh --cmd z --hook none)"
+
+## For completions
+_zoxide_zsh_tab_completion() {
+    (( $+compstate )) && compstate[insert]=menu
+    local keyword="${words:2}"
+    local completions=(${(@f)"$(zoxide query -l "$keyword")"})
+
+    if [[ ${#completions[@]} == 0 ]]; then
+        _files -/
+    else
+        compadd -U -V z "${(@)completions}"
+    fi
+}
+
+if [ "${+functions[compdef]}" -ne 0 ]; then
+    compdef _zoxide_zsh_tab_completion z 2> /dev/null
+fi
+
+
+## For auto add
+alias zdd='zoxide add `pwd`'
